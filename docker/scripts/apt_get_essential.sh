3081f73e222f374b2879a1bd495fea58279b8bf4
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2024-05-02 16:21:22 +0200

    remove unused files (#15)

--- docker/scripts/apt_get_essential.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y  --no-install-recommends \
-    apt-utils=2.0.9 \
-    ca-certificates=20211016ubuntu0.20.04.1 \
-    curl=7.68.0-1ubuntu2.15 \
-    && \
-    update-ca-certificates && \
-    apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
